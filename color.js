bc51eda19d12a732211f5394d7bec9f66c091046
Shapkka66 <kate.scan666@gmail.com>
2026-05-06 02:58:11 +0000

    Wed, May 6, 2026, 9:58 AM +07:00

--- color.js
+++ color.js
@@ -4,4 +4,4 @@ document.addEventListener('DOMContentLoaded', function ()
 
     document.querySelector('#green').addEventListener('click', function(){body.style.backgroundColor='green';});
     document.querySelector('#aqua').addEventListener('click', function(){body.style.backgroundColor='aqua';});
-}));
+});
